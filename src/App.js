713ac79099b2271e863690cab7a1f3587fa2a54b
Adam Nowak <nowak.adam2222@gmail.com>
2024-02-09 14:21:04 +0100

    added localStorage task saving

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import Header from "./Header";
 import List from "./List";
 import Form from "./Form";
 import Section from "./Section";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const savedTasks = JSON.parse(localStorage.getItem("tasks"));
 
 function App() {
 
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    {id: 1, content: "asdf", done: false},
-    {id: 2, content: "asfsa", done: true},
-    {id: 3, content: "asgfdsggf", done: false},
-  ]);
+  const [tasks, setTasks] = useState(savedTasks);
+
+  const saveTasks = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  useEffect(() => {
+    saveTasks();
+  }, [tasks]);
 
   const removeTask = (id) => {
     setTasks(tasks => tasks.filter(task => task.id !== id));
@@ -33,7 +39,6 @@ function App() {
 
   const toggleAllDone = () => {
     setTasks(tasks.map(task => ({...task, done: true})));
-    console.log("cock")
   };
 
   const addTask = (newTaskContent) => {
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -6,8 +6,8 @@ const Tasks = (props) => (
         {props.tasks.map(task => (
             <li className={`list--item ${(task.done && props.hideDone) ? "task__hidden" : ""}`}>
                 <span className={task.done ? "task__done" : ""}>{task.content}</span>
-                <button class="delete task__button" onClick={() => (props.removeTask(task.id))}>🗑</button>
-                <button class="done task__button" onClick={() => (props.toggleTaskDone(task.id))}>{(task.done) ? "✓" : ""}</button>
+                <button className="delete task__button" onClick={() => (props.removeTask(task.id))}>🗑</button>
+                <button className="done task__button" onClick={() => (props.toggleTaskDone(task.id))}>{(task.done) ? "✓" : ""}</button>
             </li>
         ))}
     </ul>
