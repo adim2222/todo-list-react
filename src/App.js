3f081fcc6782c818e9ac01eae7cbdd7474f8d84e
Adam Nowak <nowak.adam2222@gmail.com>
2024-05-12 16:10:10 +0200

    code cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from "./Header";
 import List from "./List";
 import Form from "./Form";
 import Section from "./Section";
-import { useTasks } from "./useTasks";
 import { useSelector } from "react-redux";
 import { selectTasks } from "./tasksSlice";
 
@@ -10,21 +9,11 @@ function App() {
 
   const { tasks } = useSelector(selectTasks);
 
-  const {
-    //tasks,
-    hideDone,
-    removeTask,
-    toggleAllDone,
-    toggleTaskDone,
-    addTask,
-    toggleHideDone,
-  } = useTasks();
-
   return (
     <div className="App">
       <Header />
-      <Section content={<Form addTask={addTask}/>} header={"Dodaj nowe zadanie"}/>
-      <Section content={<List toggleAllDone={toggleAllDone} toggleTaskDone={toggleTaskDone} removeTask={removeTask} hideDone={hideDone} toggleHideDone={toggleHideDone} tasks={tasks}/>} header={""}/>
+      <Section content={<Form />} header={"Dodaj nowe zadanie"}/>
+      <Section content={<List />}/>
     </div>
   );
 }
